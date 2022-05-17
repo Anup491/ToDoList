5c7fc1416ff518f2f2ed1bee5739e5c3b93d85a0
Anup491 <105752528+Anup491@users.noreply.github.com>
2022-05-17 23:19:44 +0100

    Delete src directory

--- src/app/security/app-user.ts
+++
@@ -1,5 +0,0 @@
-import { UserBase } from "../shared/security/user-base";
-
-
-export class AppUser extends UserBase {
-}
--- src/app/shared/security/user-base.ts
+++
@@ -1,5 +0,0 @@
-export class UserBase {
-    userId?: number;
-    userName: string;
-    password: string;
-}
